9fd1a2372aa801e998e950fbaa931ae70cca9bc7
ryantjy <ryantjy2002@gmail.com>
2026-07-13 22:11:44 +0800

    lb

---
+++ LoadBalancer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+//Worker
+class Worker {
+    private:
+    public:
+};
+
+//Requester
+class Requester {
+    private:
+    public:
+};
+
+//Balancer
+
+int main() {
+    return 0;
+}
